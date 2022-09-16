dabe94c288a3d0b5118f2f8cae7a2692c4e9a6e8
Akshay Zarodiya <akshayzarodiya@gmail.com>
2022-09-16 18:22:14 +0530

    Am I adding two more program

---
+++ Akshay_Zarodiya/dynamic_function.js
@@ -0,0 +1,13 @@
+
+      function printName(takeName){
+           console.log(takename);
+
+}
+
+     printName("Nayantara");
+
+     printName("Ram rao"); 
+
+     printName("pradhu deva");
+
+     printName("ram charan");
---
+++ Akshay_Zarodiya/functiondemo.js
@@ -0,0 +1,10 @@
+    
+     let name = "Anupama";
+
+        function showdata(){
+          console.log(name);    
+}
+
+       console.log(name);
+
+       showData();
