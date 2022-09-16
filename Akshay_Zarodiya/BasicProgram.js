8ab1028f7811430dca5ca73b9c04e4d48fc4be7f
Akshay Zarodiya <akshayzarodiya@gmail.com>
2022-09-16 17:51:53 +0530

    Gys please create a new folder in my repo for practice perpose

---
+++ Akshay_Zarodiya/BasicProgram.js
@@ -0,0 +1,14 @@
+
+
+   let myName = "Akshay";
+
+   let age = 32;
+
+   let bG = "AB+";
+
+   let height = null;
+
+   console.log(myName);
+   console.log(age);
+   console.log(bG);
+   console.log(height);
